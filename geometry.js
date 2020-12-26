25d94ba15793b08ead12ff320a4b1e9769d5b5ef
Matteo Poteras <epicdragon100000@gmail.com>
2020-12-26 20:52:56 +0200

    Fixed small problem

--- geometry.js
+++ geometry.js
@@ -1,24 +1,24 @@
-module.exports = {
-    Rectangle: class {
-        constructor(height, width) {
-            this.height = height;
-            this.width = width;
-        }
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
 
-        get area() {
-            return this.calculateArea();
-        }
-        
-        get perimeter() {
-            return this.calculatePerimeter();
-        }
+    get area() {
+        return this.calculateArea();
+    }
+    
+    get perimeter() {
+        return this.calculatePerimeter();
+    }
 
-        calculateArea() {
-            return this.height * this.width;
-        }
+    calculateArea() {
+        return this.height * this.width;
+    }
 
-        calculatePerimeter() {
-            return (this.height * 2) + (this.width * 2)
-        }
+    calculatePerimeter() {
+        return (this.height * 2) + (this.width * 2)
     }
 }
+
+module.exports = Rectangle;
