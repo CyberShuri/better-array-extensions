b71b50182f66359b4e862fecfec3aa9b6761bff7
CyberShuri <76031858+CyberShuri@users.noreply.github.com>
2021-01-10 16:13:51 +0200

    Update First.js

--- constructors/First.js
+++ constructors/First.js
@@ -2,8 +2,7 @@ Array.prototype.first = function (number) {
     try {
         if (!number) return this[0];
         if (number >= this.length) return this;
-        if (number == 0) return [];
-        if (number < 0) throw new Error('Value cannot be lower than 0.');
+        if (number <= 0) throw new Error('Value cannot be lower or equal to 0.');
 
         var array = [];
 
