dbadbee30190674a73dc068305536d3b1117da1d
Matteo Poteras <epicdragon100000@gmail.com>
2021-01-10 16:08:55 +0200

    Added exception

--- constructors/First.js
+++ constructors/First.js
@@ -2,6 +2,7 @@ Array.prototype.first = function (number) {
     try {
         if (!number) return this[0];
         if (number >= this.length) return this;
+        if (number === 0) return [];
         if (number < 0) throw new Error('Value cannot be lower than 0.');
 
         var array = [];
