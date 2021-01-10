bbe2831e1df5446e823665ddab68d87cdd6c537c
Matteo Poteras <epicdragon100000@gmail.com>
2021-01-10 16:29:41 +0200

    Fixed Last

--- constructors/Last.js
+++ constructors/Last.js
@@ -6,7 +6,7 @@ Array.prototype.last = function (number) {
 
         var array = [];
 
-        for (i = this.length - number; i > number; i--) {
+        for (i = this.length - number; i < this.length; i++) {
             array.push(this[i]);
         }
 
