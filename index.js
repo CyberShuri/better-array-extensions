8d0668b56f9f869d969bd575a9ce122ee9a4e002
Matteo Poteras <epicdragon100000@gmail.com>
2021-01-10 16:25:39 +0200

    Added last to index.js

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = {
     random: require('./constructors/Random'),
     compact: require('./constructors/Compact'),
     first: require('./constructors/First'),
+    last: require('./constructors/Last')
 }
